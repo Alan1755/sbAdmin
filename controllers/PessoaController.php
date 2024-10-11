60597d665c122f3140a1fb509eeb0c2c0884324d
alan <alan.silva.cr41@gmail.com>
2024-10-11 08:58:44 -0300

    Initial commit

---
+++ controllers/PessoaController.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once './models/Pessoa.php';
+
+class PessoaController {
+    protected $model;
+
+    function __construct(){
+        $this->model = new Pessoa();
+    }
+    function selectAll() {
+        $result = $this->model->selectAll();
+        var_dump($result);
+    }
+}
---
+++ salvar.php
@@ -0,0 +1,24 @@
+<?php
+    echo "Hello CARALHO";
+    echo "<br><br>";
+    echo "nome<br>";
+    echo $_POST["nome"];
+    echo "<br><br>";
+    echo "cpf<br>";
+    echo $_POST["cpf"];
+    echo "<br><br>";
+    echo "endereço<br>";
+    echo $_POST["endereco"];
+    echo "<br><br>";
+    echo "telefone<br>";
+    echo $_POST["telefone"];
+    echo "<br><br>";
+    echo "email<br>";
+    echo $_POST["email"];
+    echo "<br><br>";
+    echo "idade<br>"; 
+    echo $_POST["idade"];
+    echo "<br><br>";
+    echo "observação<br>";
+    echo $_POST["observacao"];
+?>
